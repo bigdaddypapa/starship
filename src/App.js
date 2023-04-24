bcc1c6fb44542520851d9b81c35b5f2fda92ed29
Papa Ndoye <pape2006ndoye@hotmail.com>
2023-04-24 09:11:51 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getStarShip } from "./sw-api";
 
 function App() {
+  const [starShips, setStarShips] = useState([]);
+
+  useEffect(() => {
+    fetchStarShips();
+  }, []);
+
+  async function fetchStarShips() {
+    const url = "https://swapi.dev/api/starships";
+    const data = await getStarShip(url);
+    console.log(data);
+    console.log(data.results);
+    const starShipCards = data.results.map((item, index) => (
+      <div className="card" key={index}>
+        <h2>{item.name}</h2>
+        
+      </div>
+    ));
+    setStarShips(starShipCards);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-bar">
+        <h1>Star Wars Warships</h1>
+      </div>
+      <div className="card-container">{starShips}</div>
     </div>
   );
 }
